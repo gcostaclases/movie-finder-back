29080801e0adaa48c0c68d19eaf8e9d77c713782
gcostaclases <gcostaclases@gmail.com>
2025-10-07 03:33:27 -0300

    Sigo intentando...

--- Backend/api/dev.js
+++ Backend/api/dev.js
@@ -1,6 +1,6 @@
 //#region  ----------- IMPORTS -----------
 // Importo la función que crea la app de express
-import createApp from "../app/app.js";
+import createApp from "../src/app/app.js";
 //#endregion ----------- IMPORTS -----------
 
 // Creo la app de express
--- Backend/api/index.js
+++ Backend/api/index.js
@@ -1,9 +1,10 @@
 //#region  ----------- IMPORTS -----------
 // Importo la función que crea la app de express
-import createApp from "../app/app.js";
+import createApp from "../src/app/app.js";
 //#endregion ----------- IMPORTS -----------
 
 // Creo la app de express
 const app = createApp();
 
 export default app;
+
